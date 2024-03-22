04272a8b8eef6fa140845e7c3be8bf859234dbda
Esteban Beltran <146997625+EstebanBeltr4n@users.noreply.github.com>
2024-03-21 19:06:05 -0500

    Update MainActivity.kt
    Se comenta codigo que crea bug en la ejecucion en el dispositivo real

--- app/src/main/java/com/example/emptocauca/MainActivity.kt
+++ app/src/main/java/com/example/emptocauca/MainActivity.kt
@@ -9,20 +9,22 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+/*
     lateinit var usuarioInput : EditText
     lateinit var contrasenaInput : EditText
     lateinit var loginBtn : EditText
-
+*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        /*
         usuarioInput = findViewById(R.id.usuario_input)
         contrasenaInput = findViewById(R.id.contrasena_input)
         loginBtn = findViewById(R.id.login_Btn)
+        
 
 
         loginBtn.setOnClickListener{
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             val contrasena = contrasenaInput.text.toString()
             Log.i("Test credenciales","Usuario: $usuario and Contrasena : $contrasena")
         }
+        */
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
